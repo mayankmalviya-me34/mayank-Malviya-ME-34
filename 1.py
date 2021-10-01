d4fe676deb6b55a63cb82b2b7c0b1f17b8fff69c
mayankmalviya-me34 <91718529+mayankmalviya-me34@users.noreply.github.com>
2021-10-01 12:26:15 +0530

    Frist commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+''' Program Objective : Write a python program to display a user entered name followed by
+                        Good Afternoon using input() function.
+    Name of Auther : Mayank Malviya
+    Date of creation : 01-10-2021
+    Date of lastmodification : 01-10-2021
+    Name of the modifing Auther:Mayank Malviya
+'''
+NAME=input("enter the name: ")
+print("Good Afternoon",NAME)
