779d8cc8edc571ed80460eb33168a1d7c24822f4
mayankmalviya-me34 <91718529+mayankmalviya-me34@users.noreply.github.com>
2021-10-10 17:51:00 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,19 @@
+myDict = {
+
+    "Sadak" : "Road", 
+
+    "Kharaab" : "Bad",
+
+    "Keet" : "Insect",
+
+    "Baagh" : "Tiger",
+
+    "Kaaju" : "cashew nut",
+
+}
+
+print("Options are : ", myDict.keys())
+
+wd = input("Enter a Word= ")
+
+print("The meaning of the word is= ", myDict[wd])
