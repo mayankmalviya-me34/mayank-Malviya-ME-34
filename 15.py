cdf36cf8a91d28d39c9ef290fa1b2b56107a516d
mayankmalviya-me34 <91718529+mayankmalviya-me34@users.noreply.github.com>
2021-10-10 17:53:53 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,31 @@
+n1 = int(input("Enter 1st number= "))
+
+n2 = int(input("Enter 2nd number= "))
+
+n3 = int(input("Enter 3rd number= "))
+
+n4 = int(input("Enter 4th number= "))
+
+if n1 > n4 :
+
+    x = n1
+
+else :
+
+    x = n4
+
+if n2 > n3 :
+
+    y = n2
+
+else : 
+
+    y = n3
+
+if x > y :
+
+    print(x, "is the greatest number in all")
+
+else :
+
+    print(y, "is the greatest number in all")
