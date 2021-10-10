b2a97a905c4c5e6780920021696c06995b7e2a36
mayankmalviya-me34 <91718529+mayankmalviya-me34@users.noreply.github.com>
2021-10-10 18:04:45 +0530

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,27 @@
+text = input("Enter the text : ")
+
+spam = False
+
+if("buy now" in text) :
+
+    spam = True
+
+elif("make a lot of money" in text) :
+
+    spam = True
+
+elif("subscribe this" in text) :
+
+    spam = True
+
+elif("click this" in text) :
+
+    spam = True
+
+if(spam) :
+
+    print("This text is spam")
+
+else :
+
+    print("This text is not spam")
